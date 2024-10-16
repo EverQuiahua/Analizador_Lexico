2fa5cbaa7de31a382986db6a21ba81621a616d66
EverQuiahua <everaldoquiahua3@gmail.com>
2024-10-15 19:00:48 -0700

    CD
    CD

--- src/main/java/com/example/analizador_lexico/semantico.java
+++ src/main/java/com/example/analizador_lexico/semantico.java
@@ -245,6 +245,7 @@ public class semantico {
 
     // -----------------------------------------------------------------------------------
     // 3. Control de alcance (Scope)
+    // HOLA HERI
 // cambios
     // Aquí va el código para verificar el alcance de las variables y funciones.
     // Se asegura que las variables y funciones sean usadas solo dentro de su alcance.
